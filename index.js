0af8d31fe49135004d8b65d3816137c5a1777e60
krishnendu basak <krisauz17@yahoo.com>
2022-10-26 23:36:48 +1100

    phase-1-array-find-method-lab is done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+// function superbowlWin(record, year, result){
+//     return (result === 'W')
+
+// }
+
+superbowlWin = (record) => {
+    const result = record.find(record => record.result ==='W');
+    return !!result ? result.year:undefined;
+}
